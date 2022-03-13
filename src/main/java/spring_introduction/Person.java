fcadf732a00f436f04344d1b5b5fb296db746f48
RadelNigma <radelnigma@yandex.ru>
2022-03-14 02:01:50 +0300

    Initial commit

---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,39 @@
+package spring_introduction;
+
+public class Person {
+    private Pet pet;
+    private String surname;
+    private int age;
+
+    public Person() {
+        System.out.println("Person bean is created");
+
+    }
+    public void callYourPet(){
+        System.out.println("Hello, my lovely Pet!");
+        pet.say();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: setSurname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: setAge");
+        this.age = age;
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: setPet");
+        this.pet = pet;
+    }
+}
