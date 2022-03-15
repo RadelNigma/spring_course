16fa980d83c5ede5065edfc56a13d12caa199ed9
RadelNigma <radelnigma@yandex.ru>
2022-03-15 16:40:27 +0300

    Radota 15.03.22

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
+//@Component
 public class Dog implements Pet{
 
 //    private String name;
@@ -18,14 +18,14 @@ public class Dog implements Pet{
     public void say(){
         System.out.println("Bow-Wow");
     }
-    @PostConstruct
-    protected void init(){
-        System.out.println("Class Dog: init method");
-    }
-    @PreDestroy
-    private void destroy(){
-        System.out.println("Class Dog: destroy method");
-    }
+//    @PostConstruct
+//    protected void init(){
+//        System.out.println("Class Dog: init method");
+//    }
+//    @PreDestroy
+//    private void destroy(){
+//        System.out.println("Class Dog: destroy method");
+//    }
 
 //    public String getName() {
 //        return name;
